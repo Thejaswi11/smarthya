0ab3ccdbcd59ff7f221c4ce995b147d319b02783
Thejaswi11 <109789161+Thejaswi11@users.noreply.github.com>
2023-02-10 12:30:25 +0530

    update

---
+++ pro1.py
@@ -0,0 +1,26 @@
+# class person:
+#     def __init__(self,name,age):
+#         self.name=Name
+#         self.age=age
+#      def myfunc(self):
+#          print("Hello my name is"+ self.name)
+#      def __str__(self):
+#     return "Name:"+ self.name +",age:"+str(self.age)
+# p1=person("jhon",36)
+# p1.myfunc()
+# print(p1)
+
+# class car:
+    # def _init_(self,brand,modle,hp,colour):
+#        self.brand=brand
+#        self.modle=modle
+#        self.hp=hp
+#        self.colour=colour
+#     def myfunc(self): 
+#        print(f"brand of the car is {self.brand} modle of the car is {self.modle} horse power of the car is {self.hp} and colour of the car is {self.colour} ")
+#     def _str_(self):
+#        return"brand: " + self.brand +", modle: " + str(self.modle)+  ",hp:"  + str(self.hp)+ ",colour" + (self.colour) 
+# p1 = car("ALTO",2009 , 270 ,"WHITE")
+# p1.myfunc()
+# print(p1)
+
